4cbc61da9b7b5250b6134d51cd2da4b939451cbf
dmquinn <danielmurphyberling@gmail.com>
2021-08-20 15:45:52 +0200

    first commit, just added first modal

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../stylesheets/SocialMedia.css";
+
+const SocialMedia = () => {
+  return (
+    <div className="socialMediaRow">
+      <i class="fab fa-instagram"></i>
+      <i class="fab fa-bandcamp"></i>
+      <i class="fab fa-facebook"></i>
+    </div>
+  );
+};
+
+export default SocialMedia;
