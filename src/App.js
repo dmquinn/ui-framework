9decad000ec62265cf2fe25562938906ea4276c4
dmquinn <danielmurphyberling@gmail.com>
2021-10-08 19:28:37 +0200

    updated colors and buttons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import { useState } from "react";
-import SocialMedia from "./components/SocialMedia";
-import BioModal from "./components/BioModal";
-import Sidebar from "./components/Sidebar";
-import "./stylesheets/App.css";
-import MainImage from "./albumImage.jpeg";
 const App = () => {
-  const [show, setShow] = useState(false);
-  const closeModal = () => {
-    setShow(false);
-  };
   return (
-    <div>
-      <SocialMedia />
-      <Sidebar show={show} setShow={setShow} />
-      <BioModal show={show} closeModal={closeModal} />
-      <img className="mainImage" alt="" src={MainImage}></img>
+    <div className="container-lg">
+      <div className="jumbotron bg-dark">
+        <div className="bg-vintage-green">HELLO</div>
+      </div>
     </div>
   );
 };
