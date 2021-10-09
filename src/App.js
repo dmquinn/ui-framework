7d54e00bb803a6dbd69b9908133287300816211d
dmquinn <danielmurphyberling@gmail.com>
2021-10-09 13:19:59 +0200

    added hover effects, width, height

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 const App = () => {
   return (
-    <div className="container-lg">
-      <div className="jumbotron bg-dark">
-        <div className="bg-vintage-green">HELLO</div>
+    <>
+      <div
+        className="container-lg"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1528502500164-ddfa429afa70?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80)",
+        }}
+      >
+        <div className="jumbotron glass-light rounded-lg h-40 hover-shadow hover-blur">
+          <h1 className="ml-2 text-white text-glass">HELLO</h1>
+        </div>
       </div>
-    </div>
+      <div className="m-2 p-1 w-10 center shadow-deep h-full hover-shadow  bg-vintage-green">
+        HELLO
+      </div>
+    </>
   );
 };
 
