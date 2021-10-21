3e8c415110eb18ac3eb857edb4251380ae5a6dec
dmquinn <danielmurphyberling@gmail.com>
2021-10-21 23:26:15 +0200

    added typography, image options

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 const App = () => {
   return (
     <>
-      <div className="row">
-        <div className="h-10 w-10 rounded bg-green"></div>
-        <div className="h-10 w-10 rounded bg-green-mute"></div>
-        <div className="h-10 w-10 rounded bg-yellow-mute"></div>
-        <div className="h-10 w-10 rounded bg-pink"></div>
-        <div className="h-10 w-10 rounded bg-red-light"></div>
+      <div className="card w-30 h-30 bg-red">
+        <img
+          className="cardImg fill"
+          src="https://images.unsplash.com/photo-1491566102020-21838225c3c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=361&q=80"
+        />
+        <div className="h-5 w-20 glass-light rounded mt--5 ml-5"></div>
       </div>
+      <ul>
+        <li className="bold">REALLY</li>
+        <li className="text-100">REALLY</li>
+        <li>REALLY</li>
+        <li>REALLY</li>
+      </ul>
     </>
   );
 };
